524c4e485a1c7620ce6a2300a658fce7c0b299b8
johnsonadam187@gmail.com <johnsonadam187@gmail.com>
2025-10-19 09:40:03 +0000

    created core structure

---
+++ models/present/sales/predict.sql
@@ -0,0 +1,12 @@
+with sales_predict as (
+    select 
+        sale_id::VARCHAR as id,
+        transaction_date::DATE as date,
+        product_category::VARCHAR as category,
+        region::VARCHAR as region, 
+        units_sold::INT as units,
+        revenue::FLOAT as revenue
+    from {{ref("core_sales_training")}}
+)
+
+select * from sales_predict
